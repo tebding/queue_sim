ce815835aff0404d12ea267c812ea70d260eca42
tyler <tyler.ebding@gmail.com>
2020-09-06 18:34:18 -0700

    updated comments with planned approach, pruned unnecessary use statement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+//main.rs
 
-use std::{env, fs, collections::VecDeque};
+use std::{env, fs};
 use queue_sim::*;
 
 fn main() {
@@ -8,6 +9,9 @@ fn main() {
     //sim: loop for 1 to n-1 processors
       //init vars
       //run sim for n processors
+        //check for dequeues, execute as necessary
+        //check for enqueues, execute as necessary
+        //repeat until done
       //calc wait time data
       //print iteration stats
 }
